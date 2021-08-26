dbe011bd1b1e249761ae95b2161e7ddf016ebf76
parkjungkwan <pakjkwan@gmail.com>
2021-08-26 21:10:48 +0900

    first commit

---
+++ consumer/src/main/java/net/zerotodev/api/consumer/SampleListener.java
@@ -0,0 +1,15 @@
+package net.zerotodev.api.consumer;
+
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleListener {
+    @RabbitListener(queues = "sample.queue")
+    public void receiveMessage(final Message message){
+        System.out.println(" ###################### ");
+        System.out.println(message.toString());
+    }
+}
---
+++ publisher/src/test/java/net/zerotodev/api/publisher/PublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package net.zerotodev.api.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
